c24894436cf332f71a22ef839640ff4ebb94a177
sumanthsai1 <s.sumanthsai1998@gmail.com>
2020-07-22 10:34:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/s3example/aws/AWSKeys.java
@@ -0,0 +1,10 @@
+package com.example.s3example.aws;
+
+import com.amazonaws.regions.Regions;
+
+public class AWSKeys {
+    protected static final String COGNITO_POOL_ID =     "eu-west-1:e6213bef-78a3-4ee4-8d10-0fa491b9fdf9";
+    protected static final Regions MY_REGION = Regions.EU_WEST_1; /*Change Region Here*/
+    public static final String BUCKET_NAME = "upload-api-cropm";
+
+}
